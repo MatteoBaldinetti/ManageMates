31b5e3191f22539e8e8a764fe3a7b6a1bdb6a372
MatteoBaldinetti <matteo.baldinetti@edu.univ-eiffel.fr>
2024-12-10 16:40:22 +0100

    Create project

---
+++ js/addProject.js
@@ -0,0 +1,23 @@
+const addProjectButton = document.getElementById("addProjectButton");
+const mainContainer = document.getElementById("mainContainer");
+
+function setupListener() {
+    addProjectButton.addEventListener("click", changeFormContainer);
+}
+
+function removeChilds(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function createProjectForm(parent) {
+    parent.innerHTML = "<form class='mt-4 p-3 w-50 lighter-grey-bg shadow-sm rounded' style='height: fit-content;'><div class='mb-3'><label for='projectName' class='form-label'>Nom du projet</label><input type='text' class='form-control' id='projectName' required></div><div class='mb-3'><label for='deadline' class='form-label'>Deadline</label><input type='date' class='form-control' id='deadline' required></div><div class='mb-3'><label for='budget' class='form-label'>Budget</label><input type='number' class='form-control' id='deadline' min='0' required></div><button type='submit' class='btn w-100 login-button text-white'>Créer le projet</button></form>"
+}
+
+function changeFormContainer() {
+    removeChilds(mainContainer)
+    createProjectForm(mainContainer)
+}
+
+window.addEventListener("load", setupListener);
