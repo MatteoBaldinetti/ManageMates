ab3497426dc83e3801d5240b317087e578852c4c
HeroHanex <51405269+HeroHanex@users.noreply.github.com>
2024-12-10 23:03:39 +0100

    Update redirect_headers.php

--- php/redirect_headers.php
+++ php/redirect_headers.php
@@ -1,5 +1,5 @@
 <?php
-    $rootfolder = "managemates";
+    $rootfolder = "managemates"; // Le nom du dossier dans le htdocs
 
     $index_php = "Location: /$rootfolder/index.php";
     $project_php = "Location: /$rootfolder/php/project.php";
