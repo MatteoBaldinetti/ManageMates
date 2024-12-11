62abcdba026f45f38afe6942c516f2abc710fc85
MatteoBaldinetti <matteo.baldinetti@edu.univ-eiffel.fr>
2024-12-11 11:39:43 +0100

    Fix display problem

--- js/displayProject.js
+++ js/displayProject.js
@@ -1,16 +1,17 @@
 function displayProject(link) {
     removeChilds(mainContainer);
-    displayProjectInfo(mainContainer, link)
+    displayProjectInfo(mainContainer, link);
 }
 
 function displayProjectInfo(element, link) {
+    mainContainer.classList.remove("d-flex");
     $.ajax({
         type: "POST",
         url: "../php/display_project_data.php",
         data: {id: link.id}
       })
         .done(function( response ) {
-            element.innerHTML = response
+            element.innerHTML = response;
             $("p.broken").html(response);
     });
     
