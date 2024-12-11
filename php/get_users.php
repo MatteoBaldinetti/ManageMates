d5f50570b351e8d6bc76d792cf1d7f0fe5bb9955
HeroHanex <51405269+HeroHanex@users.noreply.github.com>
2024-12-12 00:06:02 +0100

    Add files via upload

--- php/get_users.php
+++ php/get_users.php
@@ -2,9 +2,17 @@
     require "redirect_headers.php";
     require "connect_db.php";
 
+    $project_id = 1; // Get current project id
+
     try {
-        $req = 'SELECT username FROM user';
+        $req = 'SELECT username FROM user WHERE username NOT IN (
+                SELECT u.username FROM user AS u
+                JOIN collaboration AS c ON c.user_id = u.user_id
+                JOIN project AS p ON p.project_id = c.project_id
+                WHERE p.project_id = :project_id);';
+
         $pdoreq = $connect -> prepare($req);
+        $pdoreq -> bindParam(":project_id", $project_id, PDO::PARAM_INT);
         $pdoreq -> execute();
 
         foreach ($pdoreq as $value) {
---
+++ php/project_add_collaborator.php
@@ -0,0 +1,34 @@
+<?php
+    require "redirect_headers.php";
+    require "connect_db.php";
+
+    $collaborator = $_GET["collaborator"];
+    $project_id = 1; // Get the selected project id
+    $role = 2; // Consulter le projet
+
+    try {
+        $req = "INSERT INTO collaboration(user_id, project_id, role) VALUES (:user_id, :project_id, :role);";
+
+        $pdoreq = $connect -> prepare($req);
+        $pdoreq -> bindParam(':user_id', $collaborator, PDO::PARAM_STR);
+        $pdoreq -> bindParam(':project_id', $project_id, PDO::PARAM_INT);
+        $pdoreq -> bindParam(':role', $role, PDO::PARAM_STR);
+        $pdoreq -> execute();
+
+        $pdoreq -> setFetchMode(PDO::FETCH_ASSOC);
+
+        foreach ($pdoreq as $k) {
+            if (password_verify($password, $k["password"])) {
+                $_SESSION["user_id"] = $k["user_id"];
+                $_SESSION["username"] = $k["username"];
+                header($project_php);
+                exit;
+            }
+        }
+
+        header($index_php);
+
+    } catch(PDOException $event) {
+        echo "Error: ".$event -> getMessage()."<br/>";
+    }
+?>
