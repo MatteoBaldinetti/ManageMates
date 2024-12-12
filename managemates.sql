cdd39ae55ff22967a4963e73b67f1e0efdf313fe
HeroHanex <51405269+HeroHanex@users.noreply.github.com>
2024-12-12 23:50:39 +0100

    Add files via upload

--- managemates.sql
+++ managemates.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : jeu. 12 déc. 2024 à 22:34
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Dec 12, 2024 at 11:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,19 +18,19 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `managemates`
+-- Database: `managemates`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `backlog`
+-- Table structure for table `backlog`
 --
 
 CREATE TABLE `backlog` (
   `sprint_id` int(11) NOT NULL,
   `userstory_id` int(11) NOT NULL,
-  `task` varchar(255) DEFAULT NULL,
+  `task_id` int(11) NOT NULL,
   `priority` int(11) DEFAULT 0,
   `conception` varchar(255) DEFAULT NULL,
   `acceptance_criteria` varchar(255) DEFAULT NULL,
@@ -38,20 +38,20 @@ CREATE TABLE `backlog` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `backlog`
+-- Dumping data for table `backlog`
 --
 
-INSERT INTO `backlog` (`sprint_id`, `userstory_id`, `task`, `priority`, `conception`, `acceptance_criteria`, `status`) VALUES
-(1, 1, 'Ajouter un formulaire pour créer une nouvelle tâche', 1, 'Table tasks avec champs : id, titre, description', 'Les tâches ont un titre et une description', 'Fini'),
-(1, 2, 'Implémenter un système de glisser-déposer pour déplacer les tâches', 1, 'Glisser-déposer en JavaScript', 'Les tâches peuvent changer de colonne sans erreur', 'Fini'),
-(1, 3, 'Ajouter un module pour gérer les collaborateurs', 2, 'Table users avec champs : id, nom, rôle', 'Les utilisateurs peuvent être ajoutés/assignés à des tâches', 'Fini'),
-(1, 4, 'Développer l’exportation en PDF et l’envoie par mail', 2, 'Gestion des permissions', 'Les rôles Admin et Utilisateur sont respectés', 'En cours'),
-(1, 5, 'Créer des fonctionnalités d\'importation/exportation (CSV, JSON)', 3, 'Backend pour gérer les fichiers', 'Les données des tâches et des user stories sont exportables', 'A faire');
+INSERT INTO `backlog` (`sprint_id`, `userstory_id`, `task_id`, `priority`, `conception`, `acceptance_criteria`, `status`) VALUES
+(1, 1, 1, 1, 'Table tasks avec champs : id, titre, description', 'Les tâches ont un titre et une description', 'Fini'),
+(1, 2, 2, 1, 'Glisser-déposer en JavaScript', 'Les tâches peuvent changer de colonne sans erreur', 'Fini'),
+(1, 3, 3, 2, 'Table users avec champs : id, nom, rôle', 'Les utilisateurs peuvent être ajoutés/assignés à des tâches', 'Fini'),
+(1, 4, 4, 2, 'Gestion des permissions', 'Les rôles Admin et Utilisateur sont respectés', 'En cours'),
+(1, 5, 5, 3, 'Backend pour gérer les fichiers', 'Les données des tâches et des user stories sont exportables', 'A faire');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `collaboration`
+-- Table structure for table `collaboration`
 --
 
 CREATE TABLE `collaboration` (
@@ -61,7 +61,7 @@ CREATE TABLE `collaboration` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `collaboration`
+-- Dumping data for table `collaboration`
 --
 
 INSERT INTO `collaboration` (`user_id`, `project_id`, `role`) VALUES
@@ -70,7 +70,7 @@ INSERT INTO `collaboration` (`user_id`, `project_id`, `role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `objective`
+-- Table structure for table `objective`
 --
 
 CREATE TABLE `objective` (
@@ -80,7 +80,7 @@ CREATE TABLE `objective` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `objective`
+-- Dumping data for table `objective`
 --
 
 INSERT INTO `objective` (`objective_id`, `sprint_id`, `description`) VALUES
@@ -92,28 +92,28 @@ INSERT INTO `objective` (`objective_id`, `sprint_id`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `project`
+-- Table structure for table `project`
 --
 
 CREATE TABLE `project` (
   `project_id` int(11) NOT NULL,
   `project_name` varchar(255) NOT NULL,
   `start_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `deadline` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deadline` timestamp NULL DEFAULT NULL,
   `budget` float DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `project`
+-- Dumping data for table `project`
 --
 
 INSERT INTO `project` (`project_id`, `project_name`, `start_time`, `deadline`, `budget`) VALUES
-(1, 'Managemates', '2024-12-12 20:50:44', '2024-12-12 22:59:59', 0);
+(1, 'Managemates', '2024-12-12 19:50:44', '2024-12-12 21:59:59', 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `sprint`
+-- Table structure for table `sprint`
 --
 
 CREATE TABLE `sprint` (
@@ -124,16 +124,38 @@ CREATE TABLE `sprint` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `sprint`
+-- Dumping data for table `sprint`
 --
 
 INSERT INTO `sprint` (`sprint_id`, `project_id`, `start_time`, `deadline`) VALUES
-(1, 1, '2024-12-12 20:50:54', '2024-12-12 22:59:59');
+(1, 1, '2024-12-12 19:50:54', '2024-12-12 21:59:59');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `task_id` int(11) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `task`
+--
+
+INSERT INTO `task` (`task_id`, `description`) VALUES
+(1, 'Ajouter un formulaire pour créer une nouvelle tâche'),
+(2, 'Implémenter un système de glisser-déposer pour déplacer les tâches'),
+(3, 'Ajouter un module pour gérer les collaborateurs'),
+(4, 'Développer l’exportation en PDF et l’envoie par mail'),
+(5, 'Créer des fonctionnalités d\'importation/exportation (CSV, JSON)');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -143,7 +165,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`) VALUES
@@ -152,7 +174,7 @@ INSERT INTO `user` (`user_id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structure de la table `userstory`
+-- Table structure for table `userstory`
 --
 
 CREATE TABLE `userstory` (
@@ -162,7 +184,7 @@ CREATE TABLE `userstory` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Déchargement des données de la table `userstory`
+-- Dumping data for table `userstory`
 --
 
 INSERT INTO `userstory` (`userstory_id`, `sprint_id`, `description`) VALUES
@@ -173,116 +195,130 @@ INSERT INTO `userstory` (`userstory_id`, `sprint_id`, `description`) VALUES
 (5, 1, 'En tant qu’utilisateur, je veux pouvoir importer et exporter les données du tableau pour sauvegarder ou partager les informations du projet.');
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `backlog`
+-- Indexes for table `backlog`
 --
 ALTER TABLE `backlog`
-  ADD PRIMARY KEY (`sprint_id`,`userstory_id`),
-  ADD UNIQUE KEY `userstory_id` (`userstory_id`);
+  ADD PRIMARY KEY (`sprint_id`,`userstory_id`,`task_id`),
+  ADD UNIQUE KEY `userstory_id` (`userstory_id`),
+  ADD KEY `task_id` (`task_id`);
 
 --
--- Index pour la table `collaboration`
+-- Indexes for table `collaboration`
 --
 ALTER TABLE `collaboration`
   ADD PRIMARY KEY (`user_id`,`project_id`),
   ADD KEY `project_id` (`project_id`);
 
 --
--- Index pour la table `objective`
+-- Indexes for table `objective`
 --
 ALTER TABLE `objective`
   ADD PRIMARY KEY (`objective_id`),
   ADD KEY `sprint_id` (`sprint_id`);
 
 --
--- Index pour la table `project`
+-- Indexes for table `project`
 --
 ALTER TABLE `project`
   ADD PRIMARY KEY (`project_id`);
 
 --
--- Index pour la table `sprint`
+-- Indexes for table `sprint`
 --
 ALTER TABLE `sprint`
   ADD PRIMARY KEY (`sprint_id`),
   ADD KEY `project_id` (`project_id`);
 
 --
--- Index pour la table `user`
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
--- Index pour la table `userstory`
+-- Indexes for table `userstory`
 --
 ALTER TABLE `userstory`
   ADD PRIMARY KEY (`userstory_id`),
   ADD KEY `sprint_id` (`sprint_id`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `objective`
+-- AUTO_INCREMENT for table `objective`
 --
 ALTER TABLE `objective`
   MODIFY `objective_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT pour la table `project`
+-- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
   MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT pour la table `sprint`
+-- AUTO_INCREMENT for table `sprint`
 --
 ALTER TABLE `sprint`
   MODIFY `sprint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT pour la table `userstory`
+-- AUTO_INCREMENT for table `task`
+--
+ALTER TABLE `task`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `userstory`
 --
 ALTER TABLE `userstory`
   MODIFY `userstory_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Contraintes pour les tables déchargées
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `backlog`
+-- Constraints for table `backlog`
 --
 ALTER TABLE `backlog`
   ADD CONSTRAINT `backlog_ibfk_1` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`sprint_id`),
-  ADD CONSTRAINT `backlog_ibfk_2` FOREIGN KEY (`userstory_id`) REFERENCES `userstory` (`userstory_id`);
+  ADD CONSTRAINT `backlog_ibfk_2` FOREIGN KEY (`userstory_id`) REFERENCES `userstory` (`userstory_id`),
+  ADD CONSTRAINT `backlog_ibfk_3` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`);
 
 --
--- Contraintes pour la table `collaboration`
+-- Constraints for table `collaboration`
 --
 ALTER TABLE `collaboration`
   ADD CONSTRAINT `collaboration_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
   ADD CONSTRAINT `collaboration_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`);
 
 --
--- Contraintes pour la table `objective`
+-- Constraints for table `objective`
 --
 ALTER TABLE `objective`
   ADD CONSTRAINT `objective_ibfk_1` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`sprint_id`);
 
 --
--- Contraintes pour la table `sprint`
+-- Constraints for table `sprint`
 --
 ALTER TABLE `sprint`
   ADD CONSTRAINT `sprint_ibfk_1` FOREIGN KEY (`project_id`) REFERENCES `project` (`project_id`);
 
 --
--- Contraintes pour la table `userstory`
+-- Constraints for table `userstory`
 --
 ALTER TABLE `userstory`
   ADD CONSTRAINT `userstory_ibfk_1` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`sprint_id`);
