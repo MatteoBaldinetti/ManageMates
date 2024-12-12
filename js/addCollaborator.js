e6701350d6a25b5c9bbb9c917eb99b55bbeea43a
HeroHanex <51405269+HeroHanex@users.noreply.github.com>
2024-12-12 12:25:15 +0100

    Add files via upload

--- js/addCollaborator.js
+++ js/addCollaborator.js
@@ -11,10 +11,10 @@ function addInputCollaborator(response) {
         let newForm = document.createElement("form");
         newForm.method = "get";
         newForm.id= 'formDiv' 
-        newForm.action='../php/sprint_create.php'; //changer le fichier
+        newForm.action='../php/project_add_collaborator.php';
         newForm.class='mt-4 p-3 w-50 lighter-grey-bg shadow-sm rounded';
         newForm.style='height: fit-content; width: 50%;';
-        newForm.innerHTML = "<div class='mt-5 mb-3'><label for='collaborator' class='form-label'>Liste des utilisateurs disponible</label><select name='collaborator' id='collaborator' class='form-select' aria-label='Default select example'><option selected='selected'>Choisir un nom</options>" + response + "</select></div><button type='submit' class='btn w-100 login-button text-white'>Ajouter l'utilisateur au projet</button>";
+        newForm.innerHTML = "<div class='mt-5 mb-3'><label for='collaborator' class='form-label'>Liste des utilisateurs disponible</label><select name='collaborator' id='collaborator' class='form-select' aria-label='Default select example'><option selected='selected'>Choisir un nom</options>" + response + "</select><input type='number' class='form-control d-none' name='project_id' value='" + document.getElementById("getId").innerHTML + "'required></div><button type='submit' class='btn w-100 login-button text-white'>Ajouter l'utilisateur au projet</button>";
         containerFormSprint.appendChild(newForm);
         j = 1;
     }
